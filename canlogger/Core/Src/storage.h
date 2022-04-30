614c102abdcd2ff65bd0ba55ebd4938886df52c0
dillardr <65522278+dillardr@users.noreply.github.com>
2022-04-29 18:41:48 -0700

    Worked on storage functions
    Storage flush not actually flushed data.
    Currently have an issue with writing with two file pointers open.

--- canlogger/Core/Src/storage.h
+++ canlogger/Core/Src/storage.h
@@ -3,9 +3,13 @@
 #ifndef CANLOGGER_STORAGE_H
 #define CANLOGGER_STORAGE_H
 
-int init_storage();
+int init_storage(FATFS *FatFs);
 
-int flush_storage();
+int flush_storage(FATFS *FatFs);
+
+int flush_buf(int can_ch_slct, FIL *fil);
+
+int demount_storage();
 
 void test_generate_data();
 
