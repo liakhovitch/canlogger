381607ed57f0889de8e2a67004d7a139fd1f576b
dillardr <65522278+dillardr@users.noreply.github.com>
2022-04-30 19:47:05 -0700

    Finalized storage implmentation
    Storage should now work, and contain all needed funcitons. Functions for getting file size, and reading files haven't been tested.

--- canlogger/Core/Src/storage.c
+++ canlogger/Core/Src/storage.c
@@ -8,21 +8,35 @@
 
 extern struct circularBuffer buf1;
 extern struct circularBuffer buf2;
+FIL fil1;
+FIL fil2;
+static char can1[20] = "can1.csv";
+static char can2[20] = "can2.csv";
 
 //fres returns FR_OK(0) on
 int init_storage(FATFS *FatFs){
 	FRESULT fres;
 	HAL_Delay(100);
 	fres = f_mount(FatFs, "", 1);
-	return fres;
+	if(fres != FR_OK){
+		return fres;
+	}
+	fres = f_open(&fil1, can1, FA_WRITE | FA_OPEN_ALWAYS);
+	if(fres != FR_OK){
+		return fres;
+	}
+	fres = f_open(&fil2, can2, FA_WRITE | FA_OPEN_ALWAYS);
+	if(fres != FR_OK){
+		return fres;
+	}
+	return 0;
 }
 
-//This bare bones opens two discrete files one for each CAN
+//This clears the buffers and closes the file buffers
 //Then runs until the circularBuffers are empty
-int flush_storage(FATFS *FatFs){
+//Only Run when you are finished recording CAN data
+int flush_storage(){
     FRESULT fres;
-    FIL fil1;
-    FIL fil2;
     struct bufCell cell;
     int buf_state;
 
@@ -32,72 +46,59 @@ int flush_storage(FATFS *FatFs){
     uint8_t dat_len;
 
     //Clears buf1
-    fres = f_open(&fil1, "can1.csv", FA_WRITE | FA_OPEN_ALWAYS);
-    fres = f_open(&fil2, "can2.csv", FA_WRITE | FA_OPEN_ALWAYS);
-    if(fres == FR_OK){
-    	fres = f_lseek(&fil1, f_size(&fil1));
-    	if(fres != FR_OK){
-    		f_close(&fil1);
-    		return fres;
-    	}
-    	else{
-    	//Perform file write here.
-    	//Finish by calling f_close on fil
-    		buf_state = buf_get(&buf1, &cell);
-    		while(buf_state == 0){
-    			parse_packet(&cell, &sid, &eid, data, &dat_len);
-    			fres = f_printf(&fil1, "%4x, %4x,", sid, eid);
-    			for(int i = 0; i < dat_len; i++){
-    				fres = f_printf(&fil1, "%2x", data[i]);
-    			}
-    			f_printf(&fil1, ",\n");
-    			buf_state = buf_get(&buf1, &cell);
-    		}
-
-    	}
+    fres = f_lseek(&fil1, f_size(&fil1));
+    if(fres != FR_OK){
+    	f_close(&fil1);
+    	return fres;
     }
-    //Clears buf2
-    f_close(&fil1);
-    fres = f_open(&fil2, "can2.csv", FA_WRITE | FA_OPEN_ALWAYS);
-    if(fres == FR_OK){
-    	fres = f_lseek(&fil2, f_size(&fil2));
-    	if(fres != FR_OK){
-    		f_close(&fil2);
-    		return fres;
-    	}
-    	else{
-    	//Perform file write here.
-    	//Finish by calling f_close on fil
-    	buf_state = buf_get(&buf2, &cell);
-    	f_printf(&fil2, "%d, \n", buf_state);
+    else{
+
+    	buf_state = buf_get(&buf1, &cell);
     	while(buf_state == 0){
     		parse_packet(&cell, &sid, &eid, data, &dat_len);
-    		fres = f_printf(&fil2, "test, test,\n");
-    		fres = f_printf(&fil2, "%4x, %4x,", sid, eid);
+    		fres = f_printf(&fil1, "%x, %x,", sid, eid);
     		for(int i = 0; i < dat_len; i++){
-    			fres = f_printf(&fil2, "%2x", data[i]);
+    			fres = f_printf(&fil1, "%x", data[i]);
+    		}
+    		f_printf(&fil1, ",\n");
+    		buf_state = buf_get(&buf1, &cell);
+    	}
+   	}
+
+    //Clears buf2
+    fres = f_lseek(&fil2, f_size(&fil2));
+    if(fres != FR_OK){
+    	f_close(&fil2);
+    	return fres;
+    }
+    else{
+    	buf_state = buf_get(&buf2, &cell);
+    	while(buf_state == 0){
+    		parse_packet(&cell, &sid, &eid, data, &dat_len);
+    		fres = f_printf(&fil2, "%x, %x,", sid, eid);
+    		for(int i = 0; i < dat_len; i++){
+    			fres = f_printf(&fil2, "%x", data[i]);
     		}
     		f_printf(&fil2, ",\n");
     		buf_state = buf_get(&buf2, &cell);
-    		}
     	}
     }
-    else{
-        while (1) {
-            HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
-            HAL_Delay(500);
-            HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_SET);
-            HAL_Delay(500);
-        }
-    }
+
+
+    f_close(&fil1);
     f_close(&fil2);
+
+
     return 0;
 }
 
-int flush_buf(int can_ch_slct, FIL *fil){
+
+//Attempts to pop one value on the buffer for each buffer
+//Assumes pointers are at the end of file
+int pop_buf(){
 	FRESULT fres;
 
-	struct circularBuffer* buf_select;
+
     struct bufCell cell;
     int buf_state;
 
@@ -106,36 +107,104 @@ int flush_buf(int can_ch_slct, FIL *fil){
     uint8_t data[8];
     uint8_t dat_len;
 
-    //select can buffer
-    if(can_ch_slct == 1){
-    	buf_select = &buf1;
-    }
-    else if(can_ch_slct == 2){
-    	buf_select = &buf2;
-    }
-    else{
-    	return 1;
-    }
 
     //Flush buffer
     //Assumes file is open and points to end of the file
-	buf_state = buf_get(buf_select, &cell);
-	while(buf_state == 0){
+	buf_state = buf_get(&buf1, &cell);
+	if(buf_state == 0){
 		parse_packet(&cell, &sid, &eid, data, &dat_len);
-		HAL_Delay(10);
-		fres = f_printf(fil, "%16b, %16b,", sid, eid);
+		fres = f_printf(&fil1, "%x, %x,", sid, eid);
 		for(int i = 0; i < dat_len; i++){
-			fres = f_printf(fil, "%8b", data[i]);
+			fres = f_printf(&fil1, "%x", data[i]);
 		}
-		f_printf(fil, ",\n");
-		buf_state = buf_get(buf_select, &cell);
+		f_printf(&fil1, ",\n");
+	}
+
+	buf_state = buf_get(&buf2, &cell);
+	if(buf_state == 0){
+		parse_packet(&cell, &sid, &eid, data, &dat_len);
+		fres = f_printf(&fil2, "%x, %x,", sid, eid);
+		for(int i = 0; i < dat_len; i++){
+			fres = f_printf(&fil2, "%x", data[i]);
+		}
+		f_printf(&fil2, ",\n");
 	}
 	return fres;
 }
 
+int read_storage(uint8_t can_ch_slct, BYTE * buffer, UINT btr, UINT * br){
+	FIL *fil;
+	FRESULT fres;
+
+	if(can_ch_slct == 0){
+		fil = &fil1;
+	}
+	else if(can_ch_slct ==1){
+		fil = &fil2;
+	}
+
+	fres = f_read(fil, buffer, btr, br);
+	return fres;
+}
 
 
+//File pointer must be open to work
+int get_file_size(uint8_t can_ch_slct){
+	//Simple f_size() wrapper;
+	int file_size = 0;
+	if(can_ch_slct == 0){
+		file_size = f_size(&fil1);
+	}
+	else if(can_ch_slct == 1){
+		file_size = f_size(&fil2);
+	}
+	return file_size;
+}
 
+int delete_file(uint8_t can_ch_slct){
+	FRESULT fres;
+	if(can_ch_slct == 0){
+		fres = f_unlink(can1);
+	}
+	else if(can_ch_slct == 1){
+		fres = f_unlink(can2);
+	}
+	return fres;
+}
+
+int close_fil(){
+	f_close(&fil1);
+	f_close(&fil2);
+	return 0;
+}
+
+int open_fil(){
+	FRESULT fres;
+	fres = f_open(&fil1, can1, FA_WRITE | FA_OPEN_ALWAYS);
+	if(fres != FR_OK){
+		return fres;
+	}
+	fres = f_open(&fil2, can2, FA_WRITE | FA_OPEN_ALWAYS);
+	if(fres != FR_OK){
+		return fres;
+	}
+	return 0;
+}
+
+int find_eof(){
+	FRESULT fres;
+	fres = f_lseek(&fil1, f_size(&fil1));
+	if(fres != FR_OK){
+		f_close(&fil1);
+		return fres;
+	}
+	fres = f_lseek(&fil2, f_size(&fil2));
+	if(fres != FR_OK){
+		f_close(&fil2);
+		return fres;
+	}
+	return 0;
+}
 
 int demount_storage(){
     FRESULT fres;
--- canlogger/Core/Src/storage.h
+++ canlogger/Core/Src/storage.h
@@ -5,9 +5,27 @@
 
 int init_storage(FATFS *FatFs);
 
-int flush_storage(FATFS *FatFs);
+//This clears the buffers and closes the file buffers
+//Then runs until the circularBuffers are empty
+//Only Run when you are finished recording CAN data
+int flush_storage();
 
-int flush_buf(int can_ch_slct, FIL *fil);
+
+//Pulls one frame off of each buffer
+//Assume file pointers are at eof
+int pop_buf();
+
+int close_fil();
+
+int open_fil();
+
+int find_eof();
+
+int read_storage(uint8_t can_ch_slct, BYTE * buffer, UINT btr, UINT * br);
+
+int get_file_size(uint8_t can_ch_slct);
+
+int delete_file(uint8_t can_ch_slct);
 
 int demount_storage();
 
